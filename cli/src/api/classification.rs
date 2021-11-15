86c147c122e6f96f58e4b71453b63515444b8346
sigma-andex <sigma.andex@pm.me>
2021-11-15 15:34:04 +0000

    Refactor module structure

---
+++ cli/src/api/classification.rs
@@ -0,0 +1,33 @@
+extern crate base64;
+
+use crate::types::Result;
+use base64::encode;
+use log::info;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ClassificationRequest {
+    snippet: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Classification {
+    pub classification: String,
+}
+
+pub async fn classify(snippet: String) -> Result<Classification> {
+    let client = reqwest::Client::new();
+    let request = ClassificationRequest {
+        snippet: encode(snippet),
+    };
+    let request = serde_json::to_string(&request)?;
+    info!("Sending request {}", request);
+    let response = client
+        .post("http://localhost:8080/api/v1/classification")
+        .body(request)
+        .send()
+        .await?
+        .json::<Classification>()
+        .await?;
+    Ok(response)
+}
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,104 +1,15 @@
 extern crate clap;
 
 mod api;
+mod cmd;
+mod types;
+
+use crate::cmd::scribe;
 
 use clap::{App, SubCommand};
-use console::style;
-use console::Emoji;
-use indicatif::MultiProgress;
-use indicatif::ProgressBar;
-use indicatif::ProgressStyle;
-use std::time::Duration;
 use tokio;
-use tokio::sync::mpsc;
-use tokio::task::yield_now;
 
-#[cfg(not(windows))]
-const TICK_SETTINGS: (&str, u64) = ("⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏ ", 80);
-
-#[cfg(windows)]
-const TICK_SETTINGS: (&str, u64) = (r"+-x| ", 200);
-
-static PEN: Emoji<'_, '_> = Emoji("🖋 ", "=>");
-static CROSS_MARK: Emoji<'_, '_> = Emoji("❌ ", "X");
-static CHECK_MARK: Emoji<'_, '_> = Emoji("✔️ ", "!");
-static CLASSIFIED: Emoji<'_, '_> = Emoji("🗄️ ", "!");
-
-type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
-
-async fn scribe<'a>(matches: &clap::ArgMatches<'a>) -> Result<()> {
-    println!("{} {}", PEN, style("Scribing now...").bold().white());
-
-    let snippet = "```
-from cryptography.fernet import Fernet
-key = Fernet.generate_key()
-f = Fernet(key)
-token = f.encrypt(b\"A really secret message. Not for prying eyes.\")
-```";
-
-    let (tx, mut rx) = mpsc::channel(32);
-
-    let multi_progress = MultiProgress::new();
-
-    let count = 30 * 100;
-    let pb = multi_progress.add(ProgressBar::new(count));
-    let progressbar_style = ProgressStyle::default_spinner()
-        .tick_chars(TICK_SETTINGS.0)
-        .template(" {spinner:.blue} {msg:<30} ");
-    pb.set_style(progressbar_style);
-
-    let classification_handle = tokio::spawn(async move {
-        let classification_result = api::api::classify(snippet.to_owned()).await;
-        tx.send(classification_result).await
-    });
-    let result_handle = tokio::spawn(async move {
-        loop {
-            if let Ok(result) = rx.try_recv() {
-                let msg = match result {
-                    Ok(api::api::Classification { classification })
-                        if !String::is_empty(&classification) =>
-                    {
-                        format!(
-                            " {} {} {}",
-                            CLASSIFIED,
-                            style("Classfication sucessful:").dim().white(),
-                            style(classification).bold().white()
-                        )
-                    }
-                    _ => format!(
-                        " {} {}",
-                        CROSS_MARK,
-                        style("Classification failed").dim().white()
-                    ),
-                };
-
-                pb.finish_with_message(msg);
-                break;
-            }
-            pb.set_message(format!(
-                "{}",
-                style("Running classificiation...").dim().white()
-            ));
-            pb.inc(1);
-            std::thread::sleep(Duration::from_millis(10));
-        }
-
-        yield_now().await;
-    });
-
-    multi_progress.join();
-    let result = tokio::try_join!(classification_handle, result_handle);
-
-    match result {
-        Ok((first, second)) => {
-            println!("Got result {:?}", first);
-        }
-        Err(err) => {
-            println!("processing failed; error = {}", err);
-        }
-    }
-    Ok(())
-}
+use types::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -118,7 +29,7 @@ async fn main() -> Result<()> {
         .get_matches();
 
     if let Some(matches) = matches.subcommand_matches("scribe") {
-        let result = scribe(matches).await;
+        let result = scribe::scribe(matches).await;
     }
 
     Ok(())
